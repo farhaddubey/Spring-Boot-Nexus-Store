5af4d317c0f8428da0e26ab601c7ee3aca35d383
farhaddubey <farhaddubey003@gmail.com>
2026-05-22 14:29:04 +0530

    updating

---
+++ src/main/java/com/knightsnexus/GalaxyStoreAPI/GalaxyStoreApiApplication.java
@@ -0,0 +1,13 @@
+package com.knightsnexus.GalaxyStoreAPI;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GalaxyStoreApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GalaxyStoreApiApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/knightsnexus/GalaxyStoreAPI/GalaxyStoreApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.knightsnexus.GalaxyStoreAPI;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GalaxyStoreApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
